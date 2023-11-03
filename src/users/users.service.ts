2331db89d851b715bbe33d5921ea6c76817c8983
Alban Caré <alban.care@gmail.com>
2023-11-03 17:47:41 +0100

    Add UserDto to the service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './userDto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users: UserDto[] = [];
+
+  create(user: UserDto): UserDto {
+    this.users.push(user);
+    return user;
+  }
+
+  findAll(): UserDto[] {
+    return this.users;
+  }
+}

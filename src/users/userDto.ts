7d8ec6a4de7ab607d533a75ade85b7257b174682
Alban Caré <alban.care@gmail.com>
2023-11-03 17:31:04 +0100

    Add validation on UserPdo class

--- src/users/userDto.ts
+++ src/users/userDto.ts
@@ -1,5 +1,17 @@
+import { IsString, IsEmail, Length, IsNotEmpty } from 'class-validator';
+
 export class UserDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 20)
   readonly username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(8, 20)
   readonly password: string;
 }
